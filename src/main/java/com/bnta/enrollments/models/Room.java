dfc56d60ef11d8c953a1722625d5137e8bbefced
Thouhid Khan Majles <thouhidkhanmajles@gmail.com>
2022-05-19 14:56:51 +0100

    Room Class Update

--- src/main/java/com/bnta/enrollments/models/Room.java
+++ src/main/java/com/bnta/enrollments/models/Room.java
@@ -1,9 +1,25 @@
 package com.bnta.enrollments.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="rooms")
 public class Room {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany(mappedBy = "room")
+    private List<Room> rooms;
+
+
+
+
+
+
+
+
+
+
 }
