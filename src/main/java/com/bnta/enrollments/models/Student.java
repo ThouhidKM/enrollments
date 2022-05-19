1602a955f8c36ae44080f9f0a74cb9f26b6458ec
Thouhid Khan Majles <thouhidkhanmajles@gmail.com>
2022-05-19 14:57:24 +0100

    Student Class Update

--- src/main/java/com/bnta/enrollments/models/Student.java
+++ src/main/java/com/bnta/enrollments/models/Student.java
@@ -1,10 +1,39 @@
 package com.bnta.enrollments.models;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
 
 @Entity
 @Table(name="students")
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDate dob;
+
+    @Transient
+    private Integer age;
+
+
+    @ManyToOne
+    @JoinColumn(name="room_id")
+    private Room room;
+
+
+    public Integer getAge() {
+
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
