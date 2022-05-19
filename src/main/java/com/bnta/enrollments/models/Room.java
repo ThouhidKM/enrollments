0a35121a5ed27efcd93a2b308183925bfee07f4c
Thouhid Khan Majles <thouhidkhanmajles@gmail.com>
2022-05-19 13:52:21 +0100

    Creating tables

---
+++ src/main/java/com/bnta/enrollments/models/Room.java
@@ -0,0 +1,9 @@
+package com.bnta.enrollments.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="rooms")
+public class Room {
+}
---
+++ src/main/java/com/bnta/enrollments/models/Student.java
@@ -0,0 +1,10 @@
+package com.bnta.enrollments.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
+public class Student {
+}
